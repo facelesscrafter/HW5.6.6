4ff469163c3624803e0651c73d60b59a31ded36a
facelesscrafter <facelessmaster65536@gmail.com>
2022-02-13 20:21:23 +0300

    it for sub

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,5 +1,5 @@
 public class GitTest {
-    public static String s="never use 'rm -rf /' as root!";
+    public static String s="every time use 'rm -rf /' as root!";
     public static void main(String[] args) {
         System.out.println(s);
     }
